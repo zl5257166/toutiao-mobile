c7312055fe895fbbca2f4386c1b5a0fdcf389286
钟良 <zhongliang@nistone.com>
2021-09-14 15:20:39 +0800

    【feat】:搜索-联想建议 字符高亮效果

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,11 +1,14 @@
 <template>
    <div class="search-suggestion">
-     <van-cell v-for="(item, index) in suggestions" :key="index" icon="search" :title="item" />
+     <van-cell v-for="(item, index) in suggestions" :key="index" icon="search">
+       <div slot="title" v-html="hightLight(item)"></div>
+     </van-cell>
    </div>
 </template>
 
 <script>
 import { getSuggestions } from '@/api/search'
+import { debounce } from 'lodash'
 export default {
   name: 'SearchSuggestion',
   components: {},
@@ -23,16 +26,24 @@ export default {
   computed: {},
   watch: {
     searchText: {
-      async handler () {
+      handler: debounce(async function () {
         const { data } = await getSuggestions(this.searchText)
         this.suggestions = data.data.options
-      },
+      }, 300),
       immediate: true // 监听开始立即执行handler,若false,只有数据开始变化才执行
     }
   },
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    /**
+     * 处理高亮字符
+     */
+    hightLight (str) {
+      const reg = new RegExp(this.searchText, 'gi')
+      return str.replace(reg, `<span style="color: red">${this.searchText}</span>`)
+    }
+  }
 }
 </script>
 <style lang='scss' scoped>
